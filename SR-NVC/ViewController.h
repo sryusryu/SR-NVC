0dc1b1a44110eb38da65e325a08ab805b885765f
sryusryu <sryu0914@gmail.com>
2014-06-26 16:14:56 -0400

    Initial Commit

---
+++ SR-NVC/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SR-NVC
+//
+//  Created by Sue Ryu on 6/26/14.
+//  Copyright (c) 2014 Sue Ryu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ SR-NVC/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SR-NVC
+//
+//  Created by Sue Ryu on 6/26/14.
+//  Copyright (c) 2014 Sue Ryu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
